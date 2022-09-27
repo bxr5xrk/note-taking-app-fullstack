3746841a17c4f15493d0486112cbc679981bb52c
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-09-27 13:03:53 +0300

    added validation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,12 @@
+import { ValidationPipe } from './pipes/validation.pipe';
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 const start = async () => {
     const PORT = process.env.PORT || 4000;
     const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(new ValidationPipe())
 
     await app.listen(PORT, () =>
         console.log(`Server started on port: ${PORT}`)
